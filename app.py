4ad5bd644a179e6e06a4f73eb8ffc5f8bc985bf4
samy95-yes <samyauhsey@gmail.com>
2026-02-26 14:03:35 -0500

    add app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Bienvenue Hawkesbury")
+
+st.title("👋 Bienvenue à Hawkesbury")
+st.write("🎉 Félicitations ! Ton application Streamlit fonctionne.")
